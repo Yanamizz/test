19d7277e09a6c6b51b05272d6b1e5f7464dad865
813099230@qq.com <813099230@qq.com>
2026-01-18 21:30:08 +0800

    2026/1/18（opencv√）

--- test/test.cc
+++ test/test.cc
@@ -1,25 +1,20 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
-
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    printf("用法：DisplayImage.out <图像路径>\n");
-    return -1;
+  std::string path = "data/test.jpg";
+  if (argc > 1) {
+    path = argv[1];
   }
 
-  Mat image;
-  image = imread(argv[1], IMREAD_COLOR);
-
-  if (!image.data) {
-    printf("没有图像数据 \n");
-    return -1;
+  cv::Mat image = cv::imread(path);
+  if (image.empty()) {
+    std::cerr << "无法读取图片: " << path << " (请检查路径或文件是否存在)" << std::endl;
+    return 1;
   }
-  namedWindow("显示图像", WINDOW_AUTOSIZE);
-  imshow("显示图像", image);
 
-  waitKey(0);
-
+  cv::imshow("Image", image);
+  cv::waitKey(0);
   return 0;
 }
